403c27bea7b977201a3bc6ac6be554fc3feb2aa3
Thomas Houghton <tomhoughton888@gmail.com>
2021-03-05 15:54:17 +0000

    Added Dark Mode

--- g34-covid-analyser/src/App.js
+++ g34-covid-analyser/src/App.js
@@ -1,33 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import { Label } from './Components/Label';
-import { Card } from './Components/Card/Card';
+import { Grommet, Box, Button, Heading, dark } from "grommet";
+import { grommet } from "grommet";
+import React from 'react';
+
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <div className='container'>
-        <div className='row'>
-          <Card />
-          <Card />
-          <Card />
-        </div>
-      </div>
-    </div>
+    <Grommet full theme={grommet} themeMode={darkMode ? "dark" : "light"}>
+      <Box pad="large">
+        <Heading level="1"> Darkmode toggle</Heading>
+        <Button
+          label="Toggle Theme"
+          primary
+          alignSelf="center"
+          margin="large"
+          onClick={() => setDarkMode(!darkMode)}
+        />
+      </Box>t
+    </Grommet>
   );
 }
 
