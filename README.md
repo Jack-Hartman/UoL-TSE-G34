f6b9984c154980469a08794952fc72fc22e0f8a9
Jack-Hartman <35613882+Jack-Hartman@users.noreply.github.com>
2021-04-23 12:59:26 +0000

    Deploying to gh-pages from @ Jack-Hartman/UoL-TSE-G34@d9a0d140d9755d28afebfa756486bcdc87ce0f8b 🚀

---
+++ README.md
@@ -0,0 +1,48 @@
+G34 Covid Analyser
+=======
+
+Website now visible at https://tse.jackhartman.dev/
+
+# How to install for development:
+
+## Requirements:
+NodeJS: v12.16.3
+Python: 3.6
+
+## Front End:
+Go to git repository in the terminal
+
+Then go to the g34-covid-analyser folder
+
+Then run 'npm install'
+
+React (For UI)
+victory (for charts)
+Grommet (For UI Components)
+
+[![React-CI/CD](https://github.com/Jack-Hartman/UoL-TSE-G34/actions/workflows/node.js.yml/badge.svg)](https://github.com/Jack-Hartman/UoL-TSE-G34/actions/workflows/node.js.yml)
+[![](https://data.jsdelivr.com/v1/package/npm/chart.js/badge)](https://www.jsdelivr.com/package/npm/chart.js)
+
+## Back End:
+
+python3 main.py
+
+
+[![Python-CI/CD](https://github.com/Jack-Hartman/UoL-TSE-G34/actions/workflows/python.yml/badge.svg)](https://github.com/Jack-Hartman/UoL-TSE-G34/actions/workflows/python.yml)
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
+
+
+
+
+
+
+
+
+
+# Repo Stats
+[![GitHub contributors](https://img.shields.io/github/contributors/Jack-Hartman/UoL-TSE-G34.svg)](https://GitHub.com/Jack-Hartman/UoL-TSE-G34/graphs/contributors/)
+[![GitHub pull-requests](https://img.shields.io/github/issues-pr/Jack-Hartman/UoL-TSE-G34.svg)](https://GitHub.com/Jack-Hartman/UoL-TSE-G34/pulls/)
+[![GitHub pull-requests closed](https://img.shields.io/github/issues-pr-closed/Jack-Hartman/UoL-TSE-G34.svg)](https://GitHub.com/Jack-Hartman/UoL-TSE-G34/pulls/)
+
+
+
---
+++ g34-covid-analyser/src/App.js
@@ -0,0 +1,103 @@
+// Import CSS.
+import './App.css'; // Import styings for the application.
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import the bootstrap styles.
+
+// Import React.
+import React, { Component } from 'react'; // Import react.
+
+// Import My Components.
+import Default from './Views/Default';
+import About from './Views/About';
+import Loading from './Views/Loading';
+
+// Import Bootstrap Components (Import each component ont at a time to save space).
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+
+class App extends Component {
+	
+	constructor(props) {
+		super (props);
+		this.state = { View: 'loading'}
+
+		// This binding is neccessary to make `this` work in the callback
+		this.handleAboutClick = this.handleAboutClick.bind(this);
+		this.handleAccountClick = this.handleAccountClick.bind(this);
+		this.handleCreateClick = this.handleCreateClick.bind(this);
+		this.handleHomeClick = this.handleHomeClick.bind(this);
+	}
+
+	componentDidMount() { // Runs after compoennt has been mounted
+		setTimeout(() => {
+			this.setState(state => ({
+				View: 'default'
+			}));
+		}, 3000);
+	}
+
+	getCurrentView() {
+		return this.state;
+	}
+
+	handleNavClick(destination) {
+		// Need to see what the value is:
+		this.setState(state => ({ // set the state.
+			View: 'about'
+		}));
+	}
+
+	handleCreateClick() {
+		this.setState(state => ({
+			View: 'create'
+		}));
+	}
+
+	handleAboutClick() {
+		this.setState(state => ({
+			View: 'about'
+		}));
+	}
+
+	handleAccountClick() {
+
+	}
+
+	handleHomeClick() {
+		this.setState(state => ({
+			View: 'default'
+		}));
+	}
+	
+	render () {	
+		let ui;
+		
+		if (this.state.View === 'default') {
+			ui = <Default />
+		} else if (this.state.View === 'about') {
+			ui = <About />
+		} else if (this.state.View === 'loading') {
+			ui = <Loading />
+		}
+
+		return (
+			<>
+				<Navbar style={{ backgroundColor: '#30404D' }} className='text-white'>
+					<Navbar.Brand onClick={this.handleHomeClick}>G34 Covid Analyser</Navbar.Brand>
+					<Nav className="mr-auto">
+						<Button variant="primary">Create</Button>
+						<Button variant="info" onClick={this.handleAboutClick}>About</Button>
+						<Button variant="danger">Account</Button>
+					</Nav>
+				</Navbar>
+				
+				{/* Where the main view of the application is */}
+				{
+					ui
+				}
+			</>
+		);
+	}
+}
+
+export default App;
