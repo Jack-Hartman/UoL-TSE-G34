38fa724b4cd88ed0b76a312e12e01d38387e9a95
Thomas Houghton <tomhoughton888@gmail.com>
2021-05-03 17:34:11 +0100

    Comparison Works
    Ish can only select up to 4 countries before is glitches

--- g34-covid-analyser/src/Views/Compare.js
+++ g34-covid-analyser/src/Views/Compare.js
@@ -9,12 +9,81 @@ import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 
+import {
+    VictoryChart,
+    VictoryGroup,
+    VictoryBar
+} from 'victory';
+
+
+
 class Compare extends Component {
 
     constructor(props) {
         super (props);
 
         this.state = { Countries: ['none'], SelectedCountries: [] };
+
+        
+        // For all of the chart colors.
+        this.colors = [
+            [ '#388087', '#6fb3b8' ],
+            [ '#5D7529', '#DBF7A0' ],
+            [ '#36752E', '#B3F7AA' ],
+            [ '#317546', '#B1F7C6' ],
+            [ '#2F745D', '#AFF7DF' ],
+            [ '#347175', '#B6F2F7' ],
+            [ '#374775', '#BECCF7' ],
+            [ '#4C3374', '#CFB6F7' ],
+            [ '#603074', '#E1AFF7' ],
+            [ '#75276F', '#F79BEF' ],
+            [ '#751930', '#F77E9C' ],
+            [ '#751D17', '#F7837B' ],
+            [ '#75311A', '#F79E80' ],
+            [ '#754E1D', '#F7C588' ],
+            [ '#755419', '#F7CA7E' ],
+            [ '#746337', '#F7E6BE' ],
+            [ '#756D1B', '#F7ED83' ],
+            [ '#A8608E', '#E1FFDE' ],
+            [ '#A87060', '#DEFFF6' ],
+            [ '#A88560', '#DEF6FF' ],
+            [ '#A89F60', '#E3DEFF' ],
+            [ '#60A8A1', '#FFE8DE' ],
+            [ '#6079A8', '#FFF5DE' ],
+            [ '#357C8F', '#F96B64' ],
+            [ '#37358F', '#F9A54F' ],
+            [ '#82358F', '#F9E272' ],
+            [ '#8F5435', '#76FA85' ],
+            [ '#906C35', '#74F6C4' ],
+            [ '#388087', '#6fb3b8' ],
+            [ '#5D7529', '#DBF7A0' ],
+            [ '#36752E', '#B3F7AA' ],
+            [ '#317546', '#B1F7C6' ],
+            [ '#2F745D', '#AFF7DF' ],
+            [ '#347175', '#B6F2F7' ],
+            [ '#374775', '#BECCF7' ],
+            [ '#4C3374', '#CFB6F7' ],
+            [ '#603074', '#E1AFF7' ],
+            [ '#75276F', '#F79BEF' ],
+            [ '#751930', '#F77E9C' ],
+            [ '#751D17', '#F7837B' ],
+            [ '#75311A', '#F79E80' ],
+            [ '#754E1D', '#F7C588' ],
+            [ '#755419', '#F7CA7E' ],
+            [ '#746337', '#F7E6BE' ],
+            [ '#756D1B', '#F7ED83' ],
+            [ '#A8608E', '#E1FFDE' ],
+            [ '#A87060', '#DEFFF6' ],
+            [ '#A88560', '#DEF6FF' ],
+            [ '#A89F60', '#E3DEFF' ],
+            [ '#60A8A1', '#FFE8DE' ],
+            [ '#6079A8', '#FFF5DE' ],
+            [ '#357C8F', '#F96B64' ],
+            [ '#37358F', '#F9A54F' ],
+            [ '#82358F', '#F9E272' ],
+            [ '#8F5435', '#76FA85' ],
+            [ '#906C35', '#74F6C4' ]
+        ]
 
     }
 
@@ -52,12 +121,18 @@ class Compare extends Component {
         });    
     }
 
+    shortenCountryWords(arg) {
+        
+    }
+
+
+
     render() {
         return(
-            <Container>
+            <Container fluid>
                 <Row>
-                    <Col>
-                    <Card className='scrollable' style={{ width: '18rem', height: '35rem' }}>
+                    <Col xs={2}>
+                    <Card className='scrollable' style={{ width: '14rem', height: '50rem' }}>
                         <ListGroup variant="flush">
                             {
                                 this.state.Countries.map((x) => {
@@ -65,19 +140,58 @@ class Compare extends Component {
                                 })
                             }
                         </ListGroup>
+                        
                     </Card>
                     </Col>
-                    <Col>
-                    <Card style={{ width: '45rem', height: '35rem' }}>
-                        <Card.Header as="h5">Featured</Card.Header>
+                    <Col xs={10}>
+                        <Container>
+                            <Card style={{ width: '60rem' }}>
+                                <Card.Header as="h5">Comparison</Card.Header>
+                                    <Card.Body>
+                                        The key with all assosiated colours goes here.
+                                    </Card.Body>
+                                    <Card.Body>
+                                        <Card.Title>Cases Compared</Card.Title>
+                                        {/* domainPadding={{ x : [20, 20] }} */}
+                                        <div style={{padding: '20px' }}>
+                                            <VictoryChart padding={{ left: 80, bottom: 40, right: 20 }} domainPadding={{ x: 50, y: 50 }}>
+                                                <VictoryGroup offset={10}
+                                                >
+                                                    {
+                                                        this.state.SelectedCountries.map((country, index) => {
+                                                            return <VictoryBar style={{ data: { fill: this.colors[index + 10][0] } }} data={[ { x: country.Name, y: country["Cases - cumulative total"] } ]}/>
+                                                        })
+                                                    }
+
+                                                </VictoryGroup>
+                                            </VictoryChart>
+                                        </div>
+                                </Card.Body>
+                            </Card>
+
+                            <Card style={{ width: '60rem' }}>
+                        <Card.Header as="h5">Comparison</Card.Header>
                             <Card.Body>
-                                <Card.Title>Special title treatment</Card.Title>
-                                <Card.Text>
-                                With supporting text below as a natural lead-in to additional content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                The key with all assosiated colours goes here.
+                            </Card.Body>
+                            <Card.Body>
+                                <Card.Title>Deaths Compared</Card.Title>
+                                <VictoryChart padding={{ left: 80, bottom: 40, right: 20 }} domainPadding={{ x: 50 }}>
+                                    <VictoryGroup offset={20}
+                                    >
+                                        
+                                        {
+                                            this.state.SelectedCountries.map((country, index) => {
+                                                return <VictoryBar style={{ data: { fill: this.colors[index + 5][0] } }} data={[{ x: country.Name, y: country["Deaths - cumulative total"]} ]}/>
+                                            })
+                                        }
+
+                                    </VictoryGroup>
+                                </VictoryChart>
+                                
                         </Card.Body>
                         </Card>
+                        </Container>
                     </Col>
                 </Row>
             </Container>
