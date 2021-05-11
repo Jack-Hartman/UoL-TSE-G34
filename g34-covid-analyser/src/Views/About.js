ad212e53647619a2d2b1bab9e13b0b0d42883e32
Thomas Houghton <tomhoughton888@gmail.com>
2021-05-11 03:33:36 +0100

    Added Some stuff to about page

--- g34-covid-analyser/src/Views/About.js
+++ g34-covid-analyser/src/Views/About.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
+// Import react bootstrap components:
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function About() {
     return (
         <Container>
-            <h1>G34 Covid Analyser</h1>
-            <Card className='text-white' style={{ width: '20rem', backgroundColor:  '#202B33'}}>
-                <Card.Title>
-                    How to install for development
-                </Card.Title>
-                <Card.Body>
-                    <h3>Front End:</h3>
-                    <p>Go to repo in terminal. 
-                        Then go to the g34-covid-analyser folder.
-                        Then run 'npm instal'
-                    </p>
-                </Card.Body>
-            </Card>
+            <Jumbotron>
+                <h1>Welcome to G34 Covid Analyser</h1>
+                <h3>Max Pittock</h3>
+                <h3>Galani Anagnostopoulou</h3>
+                <h3>Jack Hartman</h3>
+                <h3>Joeseph Murphy</h3>
+                <h3>Thomas Houghton</h3>
+            </Jumbotron>
+            
         </Container>
     );
 }
