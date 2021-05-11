009c797802ae089f4895e23ea4d56b44d7e14151
Jack Hartman <jack27pg@gmail.com>
2021-05-11 17:09:55 +0100

    Promotional video placeholder

--- g34-covid-analyser/src/Views/About.js
+++ g34-covid-analyser/src/Views/About.js
@@ -11,12 +11,26 @@ function About() {
     return (
         <Container>
             <Jumbotron>
-                <h1>Welcome to G34 Covid Analyser</h1>
+                <h1>Welcome to G34 Covid Analyser</h1><br/>
                 <h3>Max Pittock (19700422)</h3>
                 <h3>Galani Anagnostopoulou (06061790)</h3>
                 <h3>Jack Hartman (19702197)</h3>
                 <h3>Joseph Murphy (19700614)</h3>
                 <h3>Thomas Houghton (19697363)</h3>
+            </Jumbotron>
+            <Jumbotron>
+                <h1>Promotion</h1><br/>
+                <div className="video-responsive">
+                    <iframe
+                      width="100%"
+                      height="580px"
+                      src={`https://www.youtube.com/embed/dQw4w9WgXcQ`}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      title="Embedded youtube"
+                    />
+                </div>
             </Jumbotron>
             
         </Container>
