92d43b531939479be861f291af09b47468d0038d
Jack Hartman <jack27pg@gmail.com>
2021-05-11 19:01:19 +0100

    Video update

--- g34-covid-analyser/src/Views/About.js
+++ g34-covid-analyser/src/Views/About.js
@@ -24,7 +24,7 @@ function About() {
                     <iframe
                       width="100%"
                       height="580px"
-                      src={`https://www.youtube.com/embed/dQw4w9WgXcQ`}
+                      src={`https://www.youtube.com/embed/IdHhK-H0v_I`}
                       frameBorder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                       allowFullScreen
